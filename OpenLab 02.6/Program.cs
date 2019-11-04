4fadea4d76709ee7076e722c9bc07c1fe16f706c
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:32:31 +0100

    Add project files.

---
+++ OpenLab 02.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OpenLab_02._6
+{
+    class Program
+    {
+        public static bool delitelne(int x)
+        {
+            if (x % 100 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadaj číslo, ktoré chceš aby ti napísalo či je delitelné 100 ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(delitelne(x));
+        }
+    }
+}
